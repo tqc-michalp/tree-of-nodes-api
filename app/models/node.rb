d106491bfe71b2e07e64640d52845ac0ac7da594
tqc-michalp <michal.pawlak@thinqcraft.com>
2023-04-09 22:00:54 +0200

    Create data migration from CSV to database

---
+++ app/models/node.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Node < ApplicationRecord
+  has_many :children,
+           class_name: 'Node',
+           foreign_key: 'parent_id'
+  belongs_to :parent,
+             class_name: 'Node',
+             optional: true
+end
