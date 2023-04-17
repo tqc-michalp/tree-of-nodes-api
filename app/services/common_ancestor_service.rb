6e44b181ef437af3373a7a12c7186fff2ef58149
tqc-michalp <michal.pawlak@thinqcraft.com>
2023-04-17 19:47:51 +0200

    Fix CommonAncestorService calc to match issue #2 requirments

--- app/services/common_ancestor_service.rb
+++ app/services/common_ancestor_service.rb
@@ -12,12 +12,8 @@ class CommonAncestorService
     return outcome if a.nil? || b.nil?
 
     tree_of_nodes(a, nodes_a)
-    if a.id == b.id
-      outcome.merge!(root_id: a.id, lowest_common_ancestor: nodes_a.min, depth: nodes_a.size)
-    else
-      tree_of_nodes(b, nodes_b)
-      outcome.merge!(root_id:, lowest_common_ancestor:, depth:)
-    end
+    tree_of_nodes(b, nodes_b) if a.id != b.id
+    outcome.merge!(root_id:, lowest_common_ancestor:, depth:)
 
     outcome
   end
@@ -36,31 +32,33 @@ class CommonAncestorService
     @nodes_b ||= []
   end
 
-  def tree_of_nodes(node, node_a_or_b)
+  def tree_of_nodes(node, nodes_a_or_b)
+    nodes_a_or_b.push(node.id)
+
     while node.parent_id.present?
       if node.parent_id.present?
         parent = Node.find_by(id: node.parent_id)
-        node_a_or_b.push(parent.id)
+        nodes_a_or_b.push(parent.id)
         node = parent
       else
-        node_a_or_b.push(node.id)
+        nodes_a_or_b.push(node.id)
       end
     end
   end
 
   def common_nodes
-    @common_nodes ||= nodes_a & nodes_b
+    @common_nodes ||= nodes_b.present? ? nodes_a & nodes_b : nodes_a
   end
 
   def root_id
-    @root_id ||= common_nodes.first
+    @root_id ||= common_nodes.last
   end
 
   def lowest_common_ancestor
-    @lowest_common_ancestor ||= common_nodes.min
+    @lowest_common_ancestor ||= common_nodes.first
   end
 
   def depth
-    @depth ||= (nodes_a + nodes_b).uniq.size
+    @depth ||= common_nodes.size
   end
 end
